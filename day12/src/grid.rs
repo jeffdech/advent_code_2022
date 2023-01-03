66ecf2fb2786ea82f60d15e06432e0b9fbac18a7
Jeff Dech <jeff@cloudnav.net>
2023-01-02 22:05:33 -0800

    Day 12, Part I - Redone

--- day12/src/grid.rs
+++ day12/src/grid.rs
@@ -9,11 +9,22 @@ pub enum Cell {
 }
 
 impl Cell {
-    fn elevation(&self) -> usize {
+    pub fn elevation(&self) -> usize {
         match self {
             Cell::Start => 0,
             Cell::End => 25,
-            Cell::Square(n) => *n
+            &Cell::Square(n) => n
+        }
+    }
+}
+
+impl From<char> for Cell {
+    fn from(value: char) -> Cell {
+        match value {
+            'S' => Cell::Start,
+            'E' => Cell::End,
+            value if ('a'..='z').contains(&value) => Cell::Square(value as usize - 'a' as usize),
+            _ => unreachable!()
         }
     }
 }
@@ -23,31 +34,14 @@ impl fmt::Debug for Cell {
         let c = match self {
             Cell::Start => 'S',
             Cell::End => 'E',
-            Cell::Square(n) => {
-                char::from(*n as u8 + 'a' as u8)
-            }
+            Cell::Square(n) => char::from(*n as u8 + 'a' as u8)
         };
 
         write!(f, "{c}")
     }
 }
 
-impl TryFrom<char> for Cell {
-    type Error = &'static str;
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        match value {
-            'S' => Ok(Cell::Start),
-            'E' => Ok(Cell::End),
-            value if ('a'..='z').contains(&value) => {
-                let val = value as u8 - 'a' as u8;
-                Ok(Cell::Square(val as usize))
-            },
-            _ => Err("Could not parse, not valid character")
-        }
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Coord {
     x: usize,
     y: usize
@@ -63,8 +57,8 @@ impl From<(usize, usize)> for Coord {
 }
 
 pub struct Grid {
-    pub width: usize,
     pub height: usize,
+    pub width: usize,
     cells: Vec<Cell>
 }
 
@@ -72,26 +66,29 @@ impl Grid {
     pub fn parse(input: &str) -> Self {
         let height = input.lines().count();
         let width = input.lines().next().unwrap().chars().count();
-        let cells = input.lines()
-            .map(|l| {
-                l.chars()
-                    .map(|c| Cell::try_from(c).unwrap())
-            })
+        let cells: Vec<Cell> = input.lines()
+            .map(|l| l.chars().map(|c| Cell::from(c)))
             .flatten()
-            .collect::<Vec<Cell>>();
+            .collect();
 
-        Grid {
-            width,
+        Self {
             height,
+            width,
             cells
         }
     }
 
-    fn in_bounds(&self, coord: Coord) -> bool {
+    pub fn idx2coord(&self, index: usize) -> Coord {
+        let width = index % self.width;
+        let height = (index - width) / self.width;
+        Coord::from((width, height))
+    }
+
+    fn in_bounds(&self, coord: &Coord) -> bool {
         (coord.x < self.width) && (coord.y < self.height)
     }
 
-    fn cell(&self, coord: Coord) -> Option<&Cell> {
+    pub fn cell(&self, coord: &Coord) -> Option<&Cell> {
         if self.in_bounds(coord) {
             let idx = coord.y * self.width + coord.x;
             Some(&self.cells[idx])
@@ -100,187 +97,126 @@ impl Grid {
         }
     }
 
-    fn cell_mut(&mut self, coord: Coord) -> Option<&mut Cell> {
-        if self.in_bounds(coord) {
-            let idx = coord.y * self.width + coord.x;
-            Some(&mut self.cells[idx])
-        } else {
-            None
-        }       
-    }
-
-    pub fn walkable_neighbors(&self, coord: Coord) -> impl Iterator<Item = Coord> + '_ {
-        let elevation = self.cell(coord).unwrap().elevation();
+    pub fn walkable_neighbors<'a>(&'a self, coord: &'a Coord) -> impl Iterator<Item = Coord> + '_ {
+        let curr_elevation = self.cell(coord).unwrap().elevation();
         let diffs: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
         diffs.into_iter()
             .filter_map(move |(dx, dy)| {
-                Some(Coord{
+                Some(Coord {
                     x: coord.x.checked_add_signed(dx)?,
-                    y: coord.y.checked_add_signed(dy)?,
+                    y: coord.y.checked_add_signed(dy)?
                 })
             })
-            .filter(|&coord| self.in_bounds(coord))
-            .filter(move |&coord| {
-                let other_elevation = self.cell(coord).unwrap().elevation();
-                other_elevation <= elevation + 1
+            .filter(|c| self.in_bounds(c))
+            .filter(move |c| {
+                let other_elevation = self.cell(c).unwrap().elevation();
+                other_elevation <= curr_elevation + 1
             })
-    }
-
-    pub fn idx2coord(&self, idx: usize) -> Coord {
-        let x = idx % self.width;
-        let y = (idx - x) / self.width;
-        (x, y).into()
     }
 
     pub fn start(&self) -> Coord {
-        let sidx = self.cells.iter()
+        self.cells.iter()
             .enumerate()
-            .filter_map(|(idx, v)| {
-                if *v == Cell::Start {
-                    Some(idx)
+            .filter_map(|(n, &c)| {
+                if c == Cell::Start {
+                    Some(self.idx2coord(n))
                 } else {
                     None
                 }
             })
             .next()
-            .unwrap();
-        
-        self.idx2coord(sidx)
+            .unwrap()
     }
 
     pub fn end(&self) -> Coord {
-        let eidx = self.cells.iter()
+        self.cells.iter()
             .enumerate()
-            .filter_map(|(idx, v)| {
-                if *v == Cell::End {
-                    Some(idx)
+            .filter_map(|(n, &c)| {
+                if c == Cell::End {
+                    Some(self.idx2coord(n))
                 } else {
                     None
                 }
             })
             .next()
-            .unwrap();
-        
-        self.idx2coord(eidx)        
-    }
-
-    pub fn at_height(&self, height: usize) -> Vec<Coord> {
-        self.cells.iter()
-            .enumerate()
-            .filter_map(|(idx, c)| {
-                if c.elevation() == height {
-                    Some(idx)
-                } else {
-                    None
-                }
-            })
-            .map(|idx| self.idx2coord(idx))
-            .collect()
+            .unwrap()        
     }
 }
 
 impl fmt::Debug for Grid {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "{} x {}", self.width, self.height);
         for y in 0..self.height {
             for x in 0..self.width {
-                let c = self.cell((x, y).into()).unwrap();
-                write!(f, "{:?}", c)?;
+                let c = Coord::from((x, y));
+                write!(f, "{:?}", self.cell(&c).unwrap());
             }
-            writeln!(f)?;
+            writeln!(f);
         }
 
         Ok(())
     }
 }
 
-pub struct GraphRunner<'a> {
+pub struct GraphWalker<'a> {
     grid: &'a Grid,
-    distances: HashMap<Coord, usize>,
-    previous: HashMap<Coord, Option<Coord>>,
-    unvisited: HashSet<Coord>,
-    target: Coord,
     start: Coord,
+    end: Coord,
+    distances: HashMap<Coord, usize>,
+    previous: HashMap<Coord, Coord>,
+    unvisited: HashSet<Coord>
 }
 
-impl<'a> GraphRunner<'a> {
-    pub fn new(grid: &'a Grid, start: Coord) -> Self {
+impl<'a> GraphWalker<'a> {
+    pub fn new(grid: &'a Grid, start: Coord, end: Coord) -> Self {
         let coords = (0..(grid.height * grid.width))
             .map(|n| grid.idx2coord(n))
-            .collect::<Vec<_>>();
+            .collect::<Vec<Coord>>();
 
-        let mut distances: HashMap<Coord, usize> = coords.iter()
+        let mut distances = coords.iter()
             .map(|&c| (c, usize::MAX))
-            .collect();
-
-        distances.insert(grid.start(), 0);
-
-        let unvisited: HashSet<Coord> = coords.iter().cloned().collect();
-
-        let previous: HashMap<Coord, Option<Coord>> = coords.iter()
-            .map(|&c| (c, None))
-            .collect();
+            .collect::<HashMap<Coord, usize>>();
+        distances.insert(start, 0);
 
         Self {
             grid,
-            distances,
-            previous,
-            unvisited,
-            target: grid.end(),
             start,
+            end,
+            distances,
+            previous: HashMap::new(),
+            unvisited: coords.into_iter().collect()
         }
     }
 
-    pub fn find_path(&mut self) -> Option<Vec<Coord>> {
+    pub fn search(&mut self) {
         while !self.unvisited.is_empty() {
             let u = *self.unvisited.iter()
-                .min_by_key(|c| self.distances.get(c).unwrap())
+                .min_by_key(|&c| self.distances.get(c).unwrap())
                 .unwrap();
-            
-            if u == self.target {
-                break;
-            }
 
             self.unvisited.remove(&u);
 
-            let next_points = self.grid.walkable_neighbors(u).filter(|c| self.unvisited.contains(&c));
-            for v in next_points {
-                let alt = *self.distances.get(&u).unwrap() + 1;
+            for v in self.grid.walkable_neighbors(&u) {
+                let alt = self.distances.get(&u).unwrap() + 1;
                 if alt < *self.distances.get(&v).unwrap() {
+                    self.previous.insert(v, u);
                     self.distances.insert(v, alt);
-                    self.previous.insert(v, Some(u));
                 }
             }
         }
-
-        self.path()
     }
 
-    fn path(&self) -> Option<Vec<Coord>> {
+    pub fn path(&self) -> Vec<Coord> {
         let mut p = Vec::new();
-        let mut u = Some(self.target);
+        let mut u = self.end;
 
-        if let None = self.previous.get(&u.unwrap()).unwrap() {
-            return None;
+        p.push(u);
+
+        while u != self.start {
+            u = *self.previous.get(&u).unwrap();
+            p.push(u);
         }
 
-        while (u != Some(self.start)) && (u != None) {
-            p.push(u.unwrap());
-            u = *self.previous.get(&u.unwrap()).unwrap();
-        }
-
-        if p.is_empty() {
-            return None;
-        }
-
-        Some(p.into_iter().rev().collect())
-    }
-
-    fn dump_grid(&self) {
-        for y in 0..self.grid.height {
-            for x in 0..self.grid.width {
-                let coord = Coord::from((x, y));
-
-            }
-        }
+        p.into_iter().rev().collect()
     }
 }
--- day12/src/main.rs
+++ day12/src/main.rs
@@ -2,25 +2,15 @@ mod grid;
 use grid::*;
 
 fn main() {
-    let grid = Grid::parse(include_str!("input.txt"));
-    println!("{:?}", grid);
+    let grid = Grid::parse(include_str!("sample-input.txt"));
+    println!("{grid:?}");
+    println!("{:?}", grid.cell(&Coord::from((3, 2))));
 
+    let mut walker = GraphWalker::new(&grid, grid.start(), grid.end());
+    walker.search();
+    
+    let path = walker.path();
+    println!("{:?}", path);
     println!("==== Part I ====");
-    let mut runner = GraphRunner::new(&grid, grid.start());
-    let path = runner.find_path();
-    println!("The minimum distance is {}", path.unwrap().len());
-
-    // println!("==== Part II ====");
-    // println!("There are {} cells at height 0", grid.at_height(0).len());
-
-    // let min_dist = grid.at_height(0).iter()
-    //     .enumerate()
-    //     .map(|(idx, &coord)| {
-    //         println!("Running grid {idx}");
-    //         GraphRunner::new(&grid, coord).find_path()
-    //     })
-    //     .filter_map(|p| if p == None { None } else { Some(p.unwrap().len() - 1) })
-    //     .min()
-    //     .unwrap();
-    // println!("The minimum distance is {}", min_dist);
+    println!("The length is {}", path.len() - 1);
 }
